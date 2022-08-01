c46430b6bf0d9e9edab03a2d12eced34267a9229
sunleshuai.sunlesh <sunleshuai.sunlesh@alibaba-inc.com>
2022-08-01 09:56:29 +0800

    2022.08.01

--- 6. Distributed/Code/hotel-admin/src/main/java/cn/itcast/hotel/service/IHotelService.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.hotel.service;
-
-import cn.itcast.hotel.pojo.Hotel;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface IHotelService extends IService<Hotel> {
-
-}
--- 6. Distributed/Code/learn_es_heima_hotel/src/main/java/com/sls/learn_es_heima_hotel/service/HotelService.java
+++
@@ -1,8 +0,0 @@
-package com.sls.learn_es_heima_hotel.service;
-
-import com.sls.learn_es_heima_hotel.vo.PageResult;
-import com.sls.learn_es_heima_hotel.vo.RequestParams;
-
-public interface HotelService {
-    PageResult search(RequestParams params);
-}
